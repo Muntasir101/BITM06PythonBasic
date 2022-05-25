b0be6e06346576a2c0b697b5899cbce5bf3f14d6
Sadia-Hasan123 <72305910+Sadia-Hasan123@users.noreply.github.com>
2022-05-25 12:22:40 +0600

    Modified in Dictionary

--- Sadia/Dictionary.py
+++ Sadia/Dictionary.py
@@ -1,34 +1,111 @@
-dict = {
+thisdict = {
     "Brand": "Ford",
     "Model": "Mustang",
     "Year": 1964,
     "Year": 1977
 }
-print(dict)
+print(thisdict)
 
-print(dict["Brand"])
+print(thisdict["Brand"])
 
-print(len(dict))
+print(len(thisdict))
 
-print(type(dict))
+print(type(thisdict))
 
 # Accessing Dictionary
 
-x = dict["Model"]
+x = thisdict["Model"]
 print(x)
-y = dict.get("Model")  # Accessing by get method
+y = thisdict.get("Model")  # Accessing by get method
 print(y)
 
 # Get a list of the updated keys:
 
-print(dict.keys())
-a = dict.keys()
+print(thisdict.keys())
+a = thisdict.keys()
 print(a)  # before the change
-dict["color"] = "white"
+thisdict["color"] = "white"
 print(a)  # after change
 
 # get a list of the updated values:
 
-print(dict.values())  # before the change
-dict["Year"] = 2020
-print(dict.values())  # after the change
+print(thisdict.values())  # before the change
+thisdict["Year"] = 2020
+print(thisdict.values())  # after the change
+
+# updating by update()
+thisdict.update({"Model": "Puma"})
+print(thisdict)
+
+# Check if key exist
+if "Model" in thisdict:
+    print("Yes, 'Model' is one of the keys in the dict dictionary")
+
+# Removing items
+thisdict.pop("Model")
+print(thisdict)
+
+# Print all Key names in Dictionary
+
+for x in thisdict:
+    print(x)
+
+for x in thisdict.keys():  # Using key() method
+    print(x)
+
+# Print all Values in Dictionary
+for x in thisdict:
+    print(thisdict[x])
+
+for x in thisdict.values():  # using values() method
+    print(x)
+
+# Copy dictionary using copy() method
+
+mydict = thisdict.copy()
+print(mydict)
+
+# Copy dictionary using dict() method
+
+mydict = dict(thisdict)
+print(mydict)
+
+# Nested dictionary
+myfamily = {
+    "child1": {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
+}
+
+print(myfamily)
+
+# Add three dictionary into a new dictionary
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+
+print(myfamily)
